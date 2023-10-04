3b7a8aa806d2eb756b9c74fbd7c386a9c3b228a7
augusto-roct <augusto.roct@gmail.com>
2023-10-04 18:27:41 -0300

    fix: path log

--- gateway_llms/app/utils/logs.py
+++ gateway_llms/app/utils/logs.py
@@ -8,14 +8,14 @@ class LogApplication:
         cookies = request.cookies
         headers = request.headers
         self.method = request.method
-        self.path_params = request.path_params
+        self.path = request.scope.get("path")
         query_params = request.query_params
         self.id = uuid.uuid4()
 
-        self.message = f"ID: {self.id}, IP: {self.ip}, Method: {self.method}, "
-        f"path: {self.path_params}"
+        self.message = f"ID: {self.id}, IP: {self.ip}, " \
+            f"Method: {self.method}, path: {self.path}, "
 
-        print(self.message + f" Headers: {headers}")
-        print(self.message + f" Cookies: {cookies}")
-        print(self.message + f" Body: {body}")
-        print(self.message + f" Query Params: {query_params}")
+        print(self.message + f"Headers: {headers}")
+        print(self.message + f"Cookies: {cookies}")
+        print(self.message + f"Body: {body}")
+        print(self.message + f"Query Params: {query_params}")
