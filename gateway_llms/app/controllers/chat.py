098b97bfa22db396311d00fa667c5f5f0b8a2bf0
augusto-roct <augusto.roct@gmail.com>
2023-10-08 11:36:13 -0300

    add: call openai chatcompletion

--- gateway_llms/app/controllers/chat.py
+++ gateway_llms/app/controllers/chat.py
@@ -1,15 +1,34 @@
-from gateway_llms.app.interfaces.chat import ChatLLMCompletion
-from gateway_llms.app.modules.chat.messages import get_similarity_messages_historical
+from gateway_llms.app.interfaces.chat import ChatLLMCompletion, ChatMessages
+from gateway_llms.app.modules.chat.similarity import (
+    get_similarity_messages_historical
+)
+from gateway_llms.app.modules.models.api_openai import openai_chat_completion
 from gateway_llms.app.utils.logs import LogApplication
 
 
-async def get_chat_completion(
+async def chat_completion(
     chat_llm_completion: ChatLLMCompletion,
     log_user: LogApplication
 ):
     messages_similarity = await get_similarity_messages_historical(
-        chat_llm_completion.text,
-        chat_llm_completion.messages,
+        chat_llm_completion,
         log_user
     )
-    return "Ok"
+
+    messages_similarity.append({
+        "role": "user",
+        "content": chat_llm_completion.text
+    })
+
+    content = await openai_chat_completion(
+        chat_llm_completion.model,
+        messages_similarity,
+        log_user
+    )
+
+    chat_llm_completion.messages.append(ChatMessages(
+        role="assistant",
+        content=content
+    ))
+
+    return chat_llm_completion.messages
---
+++ gateway_llms/app/modules/chat/similarity.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+from gateway_llms.app.interfaces.chat import ChatLLMCompletion
+from gateway_llms.app.modules.chat.embeddings import get_embeddings
+from gateway_llms.app.utils.logs import LogApplication
+
+
+async def get_similarity_messages_historical(
+    chat_llm_completion: ChatLLMCompletion,
+    log_user: LogApplication
+):
+    list_index_messages = []
+    messages_assistant = []
+
+    index_system = 0
+
+    for index, message in enumerate(chat_llm_completion.messages):
+        if message.role == "system":
+            index_system = index
+
+        if message.role == "assistant":
+            list_index_messages.append(index)
+            messages_assistant.append(message.content)
+
+    embeddings_user = await get_embeddings(chat_llm_completion.text, log_user)
+    embeddings_assistant = await get_embeddings(messages_assistant, log_user)
+
+    del messages_assistant
+
+    similarity = np.dot(embeddings_assistant, embeddings_user)
+    similarity = np.argsort(similarity)[:3]
+
+    del embeddings_assistant
+    del embeddings_user
+
+    messages_similarity = []
+
+    if chat_llm_completion.messages[index_system].role == "system":
+        messages_similarity.append({
+            "role": "system",
+            "content": chat_llm_completion.messages[index_system].content
+        })
+
+    for index in similarity:
+        messages_similarity.append({
+            "role": "user",
+            "content": chat_llm_completion.messages[index-1].content
+        })
+        messages_similarity.append({
+            "role": "assistant",
+            "content": chat_llm_completion.messages[index].content
+        })
+
+    return messages_similarity
